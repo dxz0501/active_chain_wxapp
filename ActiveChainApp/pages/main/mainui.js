c18bb5599480815b73973c1fc8bd4c178257369b
dimitri <duxingzhou@hotmail.com>
2020-03-23 21:13:02 +0800

    解决首页第一次进入不刷新问题

--- ActiveChainApp/pages/main/mainui.js
+++ ActiveChainApp/pages/main/mainui.js
@@ -22,59 +22,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      sportType: app.globalData.sportType
-    })
-    if (app.globalData.userInfo) {
-      api.userInfo = app.globalData.userInfo
-      api.hasUserInfo = true
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        isUserAuth: app.globalData.isUserAuth
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        app.globalData.userInfo = res.userInfo
-        app.globalData.isUserAuth = true
-        api.userInfo = res.userInfo
-        api.hasUserInfo = true
-        this.setData({
-          userInfo: res.userInfo,
-          isUserAuth: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          app.globalData.isUserAuth = true
-          api.userInfo = res.userInfo
-          api.hasUserInfo = true
-          this.setData({
-            userInfo: app.globalData.userInfo,
-            isUserAuth: app.globalData.isUserAuth
-          })
-        },
-        fail: res => {
-        }
-      })
-    }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
     app.userUpdateCallback = (data) => {
       console.log('app.userUpdateCallback:')
       console.log(data)
@@ -119,16 +66,80 @@ Page({
           userRank: '100+'
         })
       }
-      if(data.rankLast > 100) {
+      if (data.rankLast > 100) {
         this.setData({
           userRankLast: '100+'
         })
       }
     }
-    if (app.globalData.isUserAuth) {
-      api.userQuery(api.userOpenId, app.userQueryCallback)
-      api.statMStat(api.userOpenId, app.statMStatCallback)
+
+    this.setData({
+      sportType: app.globalData.sportType
+    })
+    if (app.globalData.userInfo) {
+      api.userInfo = app.globalData.userInfo
+      api.hasUserInfo = true
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        isUserAuth: app.globalData.isUserAuth
+      })
+      if (app.globalData.isUserAuth) {
+        api.userQuery(api.userOpenId, app.userQueryCallback)
+        api.statMStat(api.userOpenId, app.statMStatCallback)
+      }
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        app.globalData.userInfo = res.userInfo
+        app.globalData.isUserAuth = true
+        api.userInfo = res.userInfo
+        api.hasUserInfo = true
+        this.setData({
+          userInfo: res.userInfo,
+          isUserAuth: true
+        })
+        if (app.globalData.isUserAuth) {
+          api.userQuery(api.userOpenId, app.userQueryCallback)
+          api.statMStat(api.userOpenId, app.statMStatCallback)
+        }
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          app.globalData.isUserAuth = true
+          api.userInfo = res.userInfo
+          api.hasUserInfo = true
+          this.setData({
+            userInfo: app.globalData.userInfo,
+            isUserAuth: app.globalData.isUserAuth
+          })
+          if (app.globalData.isUserAuth) {
+            api.userQuery(api.userOpenId, app.userQueryCallback)
+            api.statMStat(api.userOpenId, app.statMStatCallback)
+          }
+        },
+        fail: res => {
+        }
+      })
     }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+
   },
 
   /**
